441fc6bcb38f3d3104c408e89cc9fdc48210aebe
aktiangco <113158472+aktiangco@users.noreply.github.com>
2026-03-26 01:45:35 -0700

    update on a count

--- src/components/PageViewCounter.jsx
+++ src/components/PageViewCounter.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 function PageViewCounter() {
   const [count, setCount] = useState("...");
 
+  const getPath = () => {
+    return encodeURIComponent(window.location.hash || "#/");
+  };
+
   useEffect(() => {
     const fetchCount = () => {
-      const path = encodeURIComponent(
-        window.location.pathname +
-          window.location.search +
-          window.location.hash
-      );
+      const path = getPath();
 
       fetch(`https://aktiangco.goatcounter.com/counter/${path}.json`)
         .then((res) => {
@@ -24,10 +24,15 @@ function PageViewCounter() {
     };
 
     fetchCount();
+
+    // update when navigating pages
     window.addEventListener("hashchange", fetchCount);
 
     return () => window.removeEventListener("hashchange", fetchCount);
   }, []);
+
+  // optional: hide while loading
+  if (count === "...") return null;
 
   return <div className="page-counter">Page Views: {count}</div>;
 }
